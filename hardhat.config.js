9c6bb63e35e5234d59539c04c9048fdc06584bc1
pglekshmi <pglekshmi88@gmail.com>
2025-03-20 15:56:08 +0530

    ⚡️| Config changed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,23 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
-  networks: {
-    localhost: {
-       url: "http://127.0.0.1:8545/"
+  defaultNetwork:'localhost',
+  networks:{
+    localhost:{
+      url:"http://127.0.0.1:8545"
+    },
+    holskey:{
+      url:"https://eth-holesky.g.alchemy.com/v2/w78nxmxF-ccuxdezBf7qeeAfjKozUIUr",
+      accounts:["597cafae381938f7eab720f4b7c0244772a556f6f1e324b6c44a98440c82ff29"]
     }
-  }
+  },
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
 };
